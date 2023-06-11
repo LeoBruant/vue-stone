f2ba861c8f4a1a0a0e8fb8f42a33939f45d84afe
LeoBruant <bruantleo@gmail.com>
2023-06-11 20:19:45 +0200

    Feat : Added game interface
    Signed-off-by: LeoBruant <bruantleo@gmail.com>

--- app/src/Play.vue
+++ app/src/Play.vue
@@ -2,31 +2,36 @@
 import Board from "@/components/Board.vue";
 import Hand from "@/components/Hand.vue";
 import { reactive } from "vue";
+import Mana from "./components/Mana.vue";
+import Player from "./components/Player.vue";
+
+const maxMana = 10;
 
 const players = reactive({
   opponent: {
-    hand: [
-      { title: "Card 1" },
-      { title: "Card 2" },
-      { title: "Card 3" },
-      { title: "Card 4" },
-    ],
-    lifepoints: 30,
+    hand: [{}, {}, {}, {}],
+    hp: 30,
     mana: 0,
     minions: [null, null, null, null, null, null, null],
+    name: "You",
   },
   self: {
     hand: [
-      { title: "Card 1" },
-      { title: "Card 2" },
-      { title: "Card 3" },
-      { title: "Card 4" },
+      { cost: 1, title: "Card 1" },
+      { cost: 2, title: "Card 2" },
+      { cost: 3, title: "Card 3" },
+      { cost: 4, title: "Card 4" },
     ],
-    lifepoints: 30,
-    mana: 0,
+    hp: 30,
+    mana: 1,
     minions: [null, null, null, null, null, null, null],
+    name: "Me",
   },
 });
+
+const turn = 1;
+
+const turnMaxMana = reactive(turn >= maxMana ? maxMana : turn);
 
 const play = (card) => {
   const index = players.self.minions.indexOf(null);
@@ -41,6 +46,25 @@ const play = (card) => {
 </script>
 
 <template>
+  <Player
+    :hp="players.opponent.hp"
+    :name="players.opponent.name"
+    position="top"
+  />
+  <Player :hp="players.self.hp" :name="players.self.name" position="bottom" />
+  <Hand :cards="players.opponent.hand" player="opponent" />
   <Board :players="players" />
-  <Hand @play="play" :cards="players.self.hand" />
+  <Hand
+    @play="play"
+    :cards="players.self.hand"
+    :mana="players.self.mana"
+    player="self"
+  />
+  <Mana :max="turnMaxMana" position="top" :value="players.opponent.mana" />
+  <Mana :max="turnMaxMana" position="bottom" :value="players.self.mana" />
+  <button
+    class="bg-yellow-300 border-2 border-yellow-700 font-bold fixed p-4 uppercase right-2 rounded top-1/2 transition-colors -translate-y-1/2 hover:bg-yellow-300/50 hover:border-yellow-700/50"
+  >
+    end turn
+  </button>
 </template>
