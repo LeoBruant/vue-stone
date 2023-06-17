76cbc6266ec42358f0f3d5e24b886c7cdd97198a
Erwan <erwan.legloannec@outlook.com>
2023-06-17 15:55:21 +0200

    Socket.io

--- api/src/main.js
+++ api/src/main.js
@@ -4,6 +4,11 @@ import cors from "cors";
 import userController from "./controller/user.js";
 import authenticationController from "./controller/authentication.js";
 import db from "./model.mjs";
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+const server = createServer(app);
+const io = new Server(server);
 
 config();
 export const app = express();
@@ -19,6 +24,14 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
 });
 
 app.use(userController);
