4922a3facaa2a2e91e2dc64ac4e22887ffa42ace
LeoBruant <bruantleo@gmail.com>
2023-07-16 21:12:15 +0200

    Fix : Branch
    Signed-off-by: LeoBruant <bruantleo@gmail.com>

--- app/src/main.js
+++ app/src/main.js
@@ -3,7 +3,6 @@ import Home from "@/Home.vue";
 import Play from "@/Play.vue";
 import Login from "@/Login.vue";
 import "@/scss/main.scss";
-import { createPinia } from 'pinia';
 import { createApp } from "vue";
 import { createRouter, createWebHashHistory } from "vue-router";
 
@@ -18,10 +17,8 @@ const router = createRouter({
   routes,
 });
 
-const pinia = createPinia()
 const app = createApp(App);
 
 app.use(router);
-app.use(pinia)
 
 app.mount("#app");
