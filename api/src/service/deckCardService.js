de30bbce2474c055654d36fec9769dfb95d85f50
Théo Dubuisson <dubuissontheo@outlook.fr>
2023-07-25 14:43:28 +0200

    :fix: eslint

--- api/src/service/deckCardService.js
+++
@@ -1,7 +0,0 @@
-import db from "../model.mjs";
-import fs from "fs/promises";
-import { join } from "path";
-
-export async function createDeckCard(body) {
-  return await db.DeckCard.create(body);
-}
--- api/src/service/deckService.js
+++
@@ -1,7 +0,0 @@
-import db from "../model.mjs";
-import fs from "fs/promises";
-import { join } from "path";
-
-export async function createDeck(body) {
-  return await db.Deck.create(body);
-}
