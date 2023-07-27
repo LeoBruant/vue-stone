2afadb834cb362b47b00da8e0df7b88f7d98fab0
Erwan <erwan.legloannec@outlook.com>
2023-07-27 09:40:38 +0200

    Fix test le retour du retour ? aka le retour 2

--- api/src/middleware/authenticate.js
+++ api/src/middleware/authenticate.js
@@ -12,7 +12,7 @@ function authenticate(req, res, next) {
 
   if (token == null) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, process.env.JWT_SECRET ?? "secret", (err, user) => {
     if (err) return res.sendStatus(403);
 
     req.user = user;
